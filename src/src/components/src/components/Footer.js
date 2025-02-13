e2ebd628ff75c78ee51bbcc81e9367d1b803e15f
slami96 <adam.slamen@gmail.com>
2025-02-13 18:59:39 +0100

    Create Footer.js

---
+++ src/src/components/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white py-6">
+      <div className="container mx-auto px-4 text-center">
+        <p>© 2025 DishDelights. All rights reserved.</p>
+        <p className="mt-2">456 Culinary Avenue, Gourmet Town, 1010 Vienna, Austria</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
