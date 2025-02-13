c1b0c5f206bc6c3ef821270f3a9faf152619997b
slami96 <adam.slamen@gmail.com>
2025-02-13 19:03:05 +0100

    Create recipes.js

---
+++ src/data/recipes.js
@@ -0,0 +1,65 @@
+export const recipes = [
+  {
+    id: 1,
+    title: "Classic Spaghetti Carbonara",
+    difficulty: "Medium",
+    prepTime: "15 mins",
+    cookTime: "20 mins",
+    servings: 4,
+    ingredients: [
+      "400g spaghetti",
+      "200g pancetta",
+      "4 large eggs",
+      "100g Pecorino Romano cheese",
+      "100g Parmigiano-Reggiano",
+      "2 cloves garlic",
+      "Salt and black pepper"
+    ],
+    instructions: [
+      "Bring a large pot of salted water to boil",
+      "Cook spaghetti according to package instructions",
+      "Meanwhile, cook pancetta until crispy",
+      "Beat eggs and mix with grated cheese",
+      "Combine hot pasta with egg mixture",
+      "Add pancetta and serve immediately"
+    ],
+    nutrition: {
+      calories: 650,
+      protein: "30g",
+      carbohydrates: "70g",
+      fat: "25g"
+    },
+    category: "Italian"
+  },
+  {
+    id: 2,
+    title: "Vegetarian Buddha Bowl",
+    difficulty: "Easy",
+    prepTime: "20 mins",
+    cookTime: "30 mins",
+    servings: 2,
+    ingredients: [
+      "1 cup quinoa",
+      "2 cups mixed vegetables",
+      "1 avocado",
+      "2 tbsp olive oil",
+      "1 cup chickpeas",
+      "Tahini dressing"
+    ],
+    instructions: [
+      "Cook quinoa according to package instructions",
+      "Roast vegetables in the oven",
+      "Prepare tahini dressing",
+      "Assemble bowls with quinoa base",
+      "Top with roasted vegetables and chickpeas",
+      "Drizzle with dressing"
+    ],
+    nutrition: {
+      calories: 450,
+      protein: "15g",
+      carbohydrates: "55g",
+      fat: "20g"
+    },
+    category: "Vegetarian"
+  }
+];
